a89b507e9d3c62f21a303ef9e184ff556a3c8796
mathbueeno <mathbueeno@gmail.com>
2022-12-26 14:31:43 -0300

    Finalização da Pokedex

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,18 @@
-import React from 'react';
+import React, { useContext } from "react";
+import FavoriteContext from "../contexts/favoritesContext";
 
 const Navbar = () => {
-    const logoImg = "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png";
+    const { favoritePokemons } = useContext(FavoriteContext);
+    const logoImg =
+        "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png";
     return (
         <nav>
             <div>
-                <img alt="pokeapi-logo"
-                    src={logoImg}
-                    className='navbar-img'
-                />
+                <img alt="pokeapi-logo" src={logoImg} className="navbar-img" />
             </div>
+            <div>{favoritePokemons.length} ❤️</div>
         </nav>
-    )
+    );
+};
 
-
-}
-
-export default Navbar
+export default Navbar;
